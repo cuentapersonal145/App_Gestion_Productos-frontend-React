a4ecd90d22544016f016374946a51a2d0ebd82a7
unknown <cuentapersonal145@gmail.com>
2024-10-10 12:45:50 -0500

    correccion ip negocio

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //const API_URL = 'http://localhost:8000';
 //const API_URL = 'http://192.168.1.51:8000';
-const API_URL = 'http://192.168.1.104:8000';
+const API_URL = 'http://192.168.1.105:8000';
 
 export default class service {
     
