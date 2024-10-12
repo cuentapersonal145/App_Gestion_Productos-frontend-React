dbcac4c585274e9458d5d667811db801b3b161a3
EDINSON <cuentapersonal145@gmail.com>
2024-10-12 13:26:17 -0500

    responsive cel Facturacion

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
-//const API_URL = 'http://localhost:8000';
-//const API_URL = 'http://192.168.1.51:8000';
-const API_URL = 'http://192.168.1.105:8000';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export default class service {
-    
+
     get_articulos_service() {
         const url = `${API_URL}/api/articulo/`;
         return axios.get(url);
